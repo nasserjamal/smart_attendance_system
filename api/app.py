c2d182d624e6a128b2ec0a9c849c3c5fdb22762b
Nasser Jamal <nasserjamal30@gmail.com>
2023-02-23 11:06:42 +0300

    api setup

---
+++ api/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello guys"
+
+if __name__ == "__main__":
+    app.run(port='5000',host='0.0.0.0', debug=True, threaded=True)
