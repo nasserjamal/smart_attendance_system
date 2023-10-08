253a9ddd1ee0fbf1a02b39e911775297c8f22b4e
Nasser Jamal <nasserjamal30@gmail.com>
2023-10-08 16:59:42 +0300

    final changes

--- ML/__init__.py
+++
@@ -1,5 +0,0 @@
-import cv2
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-faces_csv = "ML/faces.csv"
-trained_model = "ML/trainer.yml"
-recognizer = cv2.face.LBPHFaceRecognizer_create()
--- setup_mysql_dev.sql
+++
@@ -1,7 +0,0 @@
--- prepares a MySQL server for the project
-
-CREATE DATABASE IF NOT EXISTS smart_attendance;
-CREATE USER IF NOT EXISTS 'smart_attendance_dev'@'localhost' IDENTIFIED BY '12345';
-GRANT ALL PRIVILEGES ON `smart_attendance`.* TO 'smart_attendance_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'smart_attendance_dev'@'localhost';
-FLUSH PRIVILEGES;
